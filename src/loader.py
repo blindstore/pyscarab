23033286510f8cb97961b3effd9092cafba228d1
Bogdan <bogdan.kulinich@gmail.com>
2014-07-31 16:56:03 +0200

    Extend loader

--- src/loader.py
+++ src/loader.py
@@ -1,11 +1,9 @@
 """Library instance"""
 
 import os
-import ctypes
 
-
-path = os.path.abspath(__file__)
-project_path = os.path.dirname(os.path.dirname(path))
+from ctypes import cdll
+from ctypes.util import find_library
 
 
 class ScarabLoader(object):
@@ -19,7 +17,13 @@ class ScarabLoader(object):
     def __call__(self):
         """Load library if not loaded before"""
         if self.scarab is None:
-            self.scarab = ctypes.cdll.LoadLibrary(os.path.join(project_path,
-                'lib/scarab/build/libscarab.so'))
+            lib_path = find_library('scarab')
+            if lib_path is None:
+                file_path = os.path.abspath(__file__)
+                project_path = os.path.dirname(os.path.dirname(file_path))
+                default_lib_path = 'lib/scarab/build/libscarab.so'
+                lib_path = os.path.join(project_path, default_lib_path)
+
+            self.scarab = cdll.LoadLibrary(lib_path)
 
         return self.scarab
